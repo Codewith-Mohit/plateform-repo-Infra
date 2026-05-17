6fd0ff485eacaf4aab3f8407c2f275f7b113b9fb
mohit tyagi <mohit.tyagi.de@gmail.com>
2026-05-17 21:30:07 +0200

    updating node vn sku

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "agentpool"
     node_count = 1
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_B2s_v2"
   }
 
   identity {
